298ac0fa044ef6962ad4612d7aff4853dd72d13f
Seref Bolat <bolat.seref.de@gmail.com>
2022-11-13 12:34:39 +0100

    timer added

--- app/src/main/java/com/creactivestudio/mathtilt/GameActivity.java
+++ app/src/main/java/com/creactivestudio/mathtilt/GameActivity.java
@@ -3,6 +3,10 @@ package com.creactivestudio.mathtilt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -14,19 +18,27 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
-    ProgressBar progressBar;
+
+    SensorManager gyroscopeSensorManager;
+    Sensor gyroscopeSensor;
+    ProgressBar progressBar, progressBarLinear;
     Random randomNumber;
     private TextView tvTargetNumber, tvCalculation;
     CountDownTimer countDownTimer;
     int RandomTargetNumber;
+    double progress;
+    int progressBarProgress = 5;
+    int numberType=1;
+    int currentCalculationNumber=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+
         init();
         setTargetTextView();
-        startGame();
+        startCounter();
     }
 
     /**
@@ -36,7 +48,11 @@ public class GameActivity extends AppCompatActivity {
         tvTargetNumber=findViewById(R.id.tvQ);
         randomNumber=new Random();
         progressBar=findViewById(R.id.progressBar);
+        progressBarLinear=findViewById(R.id.progressBar2);
         tvCalculation=findViewById(R.id.tvCalculation);
+        gyroscopeSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyroscopeSensor=gyroscopeSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
     }
     /**
      * Return an int number for level
@@ -52,15 +68,34 @@ public class GameActivity extends AppCompatActivity {
         return returnValue;
     }
 
+
+    public int getRandomNumberForCalculation (int level) {
+        int returnValue=0;
+        if (level==1)
+        {
+            returnValue=randomNumber.nextInt(10)+1;// Starts with 1 not with 0
+        }
+        return returnValue;
+    }
+
     /**
      * Start Count Down Timer and the Game
      */
-    public void startGame () {
-        countDownTimer = new CountDownTimer(2100, 1000) {
+    public void startCounter() {
+        progressBarLinear.setVisibility(View.VISIBLE);
+       // progressBarLinear.setProgress(100);
+
+        progress = 100;
+        progressBarLinear.setProgress(progressBarProgress);
+
+        countDownTimer = new CountDownTimer(4100, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
               //  tvCountDownApp.setTextSize(100);
                // tvCountDownApp.setText(millisUntilFinished / 1000 + "");
+                progressBarProgress--;
+               // progress=progress/3;
+                progressBarLinear.setProgress((int)progressBarProgress*100/(5000/1000));
             }
 
             @Override
@@ -69,7 +104,10 @@ public class GameActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.INVISIBLE);
                 tvCalculation.setText(RandomTargetNumber+" = ");
                 tvTargetNumber.setText("");
-
+                progressBarLinear.setVisibility(View.INVISIBLE);
+                startCounter();
+                progressBarProgress=5;
+                updateRandomNumberTextView();
             }
         };
         countDownTimer.start();
@@ -83,6 +121,51 @@ public class GameActivity extends AppCompatActivity {
         RandomTargetNumber=getRandomNumberForQuestion(1);
         String targetNumberText= RandomTargetNumber+"";
         tvTargetNumber.setText(targetNumberText);
+
+    }
+
+    /**
+     *
+     */
+    public void updateRandomNumberTextView () {
+
+            currentCalculationNumber=getRandomNumberForCalculation(1);
+            tvTargetNumber.setText(String.valueOf(currentCalculationNumber));
+
+    }
+
+
+
+    private SensorEventListener gyroscopeSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            // More code goes here
+            if(sensorEvent.values[0] > 0.5f) { // anticlockwise
+               // getWindow().getDecorView().setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
+                Toast.makeText(GameActivity.this, "anti clock", Toast.LENGTH_SHORT).show();
+               // startActivity(new Intent(GameActivity.this, MathOperationsActivity.class));
+            } else if(sensorEvent.values[0] < -0.5f) { // clockwise
+               // getWindow().getDecorView().setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark));
+                Toast.makeText(GameActivity.this, " clock", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gyroscopeSensorManager.registerListener(gyroscopeSensorListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gyroscopeSensorManager.unregisterListener(gyroscopeSensorListener);
 
     }
 
---
+++ app/src/main/java/com/creactivestudio/mathtilt/MathOperationsActivity.java
@@ -0,0 +1,14 @@
+package com.creactivestudio.mathtilt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MathOperationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_operations);
+    }
+}
